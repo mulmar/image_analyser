6e54ad9b8e02f3e07f215b679dde6fc4c59bc190
Martin Mulder <martinmulder@me.com>
2020-06-07 07:52:43 +0200

    it's all working locally again

--- src/components/signin/Signin.js
+++ src/components/signin/Signin.js
@@ -19,7 +19,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://mighty-wave-62291.herokuapp.com/signin', {
+    fetch('http://localhost:3000/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
